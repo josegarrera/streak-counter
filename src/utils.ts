cfecff3711dcd3de5dd2c9ee198246e30a2736e2
josegarrera <josegarrera@gmail.com>
2022-03-21 10:53:12 -0300

    increment and reset functionalities

--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,35 @@
+// Used when storing in localStorage
+const KEY = 'streak';
+
 export function formattedDate(date: Date): string {
 	return date.toLocaleDateString('en-US');
+}
+export interface Streak {
+	currentCount: number;
+	startDate: string;
+	lastLoginDate: string;
+}
+
+export function buildStreak(
+	date: Date,
+	overrideDefaults?: Partial<Streak>
+): Streak {
+	const defaultStreak = {
+		currentCount: 1,
+		startDate: formattedDate(date),
+		lastLoginDate: formattedDate(date),
+	};
+
+	return {
+		...defaultStreak,
+		...overrideDefaults,
+	};
+}
+
+export function getStreak(storage: Storage): string | null {
+	return storage.getItem(KEY);
+}
+
+export function updateStreak(storage: Storage, streak: Streak): void {
+	storage.setItem(KEY, JSON.stringify(streak));
 }
